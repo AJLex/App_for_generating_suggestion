78767746d93ec9da0d963ce7d257a9267dd488ac
Aleksei Shuliatev <ashuliatyev@gmail.com>
2017-09-14 17:57:08 +0300

    more faster search, only for len > 1

--- adding_end_to_word.py
+++ adding_end_to_word.py
@@ -16,12 +16,16 @@ def get_input_data(filename):
         words_beginning = []
         for raw in reader:
             raw_data = [raw.get('word').lower(), raw.get('Doc')]
-            if raw_data[0][0] in rus_freq_dict.keys():
-                rus_freq_dict[raw_data[0][0]][raw_data[0]] =  raw_data[1]
-            else:
-                rus_freq_dict[raw_data[0][0]] = {raw_data[0]: raw_data[1]}
+            if len(raw_data[0]) > 1:
+                if raw_data[0][0] in rus_freq_dict.keys():
+                    if raw_data[0][0:2] in rus_freq_dict[raw_data[0][0]].keys():
+                        rus_freq_dict[raw_data[0][0]][raw_data[0][0:2]][raw_data[0]] = raw_data[1]
+                    else:
+                        rus_freq_dict[raw_data[0][0]][raw_data[0][0:2]] = {raw_data[0]: raw_data[1]}
+                else:
+                    rus_freq_dict[raw_data[0][0]] = {raw_data[0][0:2]: {raw_data[0]: raw_data[1]}}
             words_beginning.append(raw_data[0][:random.randint(1, len(raw_data[0]))])
-    return rus_freq_dict, words_beginning[:10000]
+    return rus_freq_dict, words_beginning[:50000]
 
 
 def suggest_options(input_data):
@@ -29,13 +33,16 @@ def suggest_options(input_data):
     count = 0
     for words_beginning in input_data[1]:
         count += 1
-        print(count, len(input_data[1]))
+        # print(count, len(input_data[1]))
         list_of_options = []
-        for letter in input_data[0].keys():
-            if words_beginning[0] == letter:
-                for word, freq in input_data[0][letter].items():
-                    if words_beginning == word[:len(words_beginning)]:
-                        list_of_options.append((word, freq))
+        if len(words_beginning) > 1:
+            for letter in input_data[0].keys():
+                if words_beginning[0] == letter:
+                    for letters in input_data[0][letter].keys():
+                        if words_beginning[0:2] == letters:
+                            for word, freq in input_data[0][letter][letters].items():
+                                if words_beginning == word[:len(words_beginning)]:
+                                    list_of_options.append((word, freq))
         list_of_options.sort(key=lambda word_info: word_info[1], reverse=True)
         suggest_options_dict[words_beginning] = list_of_options[:10]
     return suggest_options_dict
@@ -43,5 +50,3 @@ def suggest_options(input_data):
 
 if __name__ == '__main__':
     print(suggest_options(get_input_data('freqrnc2011.csv')))
-    # for letter, word_info in get_input_data('freqrnc2011.csv')[0].items():
-    #     print(letter, word_info)
