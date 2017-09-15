e47a844ff142980e3c23791ddc0ddded6a64d975
Aleksei Shuliatev <ashuliatyev@gmail.com>
2017-09-15 12:14:55 +0300

    binary search

--- adding_end_to_word.py
+++ adding_end_to_word.py
@@ -17,8 +17,12 @@ def get_input_data(filename):
         for raw in reader:
             raw_data = [raw.get('word').lower(), raw.get('Doc')]
             rus_freq_dict[raw_data[0]] = raw_data[1]
-            prefixes.append(raw_data[0][:random.randint(1, len(raw_data[0]))])
-    return rus_freq_dict, prefixes[:100]
+            if len(raw_data[0]) > 2:
+                max_len = len(raw_data[0]) - 1
+            else:
+                max_len = len(raw_data[0])
+            prefixes.append(raw_data[0][:random.randint(1, max_len)])
+    return rus_freq_dict, prefixes[:10000]
 
 
 def suggest_options(input_data):
@@ -31,20 +35,22 @@ def suggest_options(input_data):
         list_of_options = []
         left_limit = 0
         right_limit = len(words)
-        middle = int(left_limit/2 + right_limit/2)
-        for word_index in range(left_limit, right_limit):
-            if words[word_index].startswith(prefix):
-                if word_index > middle:
-                    left_limit = middle
-                else:
-                    right_limit = middle
-        for index in range(left_limit, right_limit):
+        while left_limit < right_limit:
+            middle = int(left_limit/2 + right_limit/2)
+            if prefix > words[middle]:
+                left_limit = middle + 1
+            else:
+                right_limit = middle
+        # print(prefix, words[left_limit])
+        for index in range(left_limit, len(words)):
             if prefix == words[index][:len(prefix)]:
                 list_of_options.append((words[index], input_data[0].get(words[index])))
+            else:
+                break
         suggest_options_dict[prefix] = list_of_options[:10]
     return suggest_options_dict
 
 
 if __name__ == '__main__':
-    print(suggest_options(get_input_data('freqrnc2011.csv')))
+    suggest_options(get_input_data('freqrnc2011.csv'))
     # print(get_input_data('freqrnc2011.csv'))
