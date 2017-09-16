92dfe355f4b3e2250a81744bd15f569767332ee5
Aleksei Shuliatev <ashuliatyev@gmail.com>
2017-09-16 23:00:04 +0300

    added functions

--- client.py
+++ client.py
@@ -2,28 +2,40 @@ import socket
 import sys
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = input('Enter hostname:\n> ')
-server_port =int(input('Enter server port:\n> '))
-port = (host, server_port)
-s.connect(port)
-data = s.recv(1024)
-print (data.decode('utf-8'))
-data = s.recv(1024)
-print (data.decode('utf-8'))
-s.send(bytes('Client connected', encoding='utf-8'))
-while True:
-    data = input("> ")
-    s.send(bytes(data, encoding='utf-8'))
-    if data == 'exit':
-        break
-    else:
-        while True:
-            word = s.recv(2048).decode('utf-8')
-            if word[-3:] == 'end':
-                print(word[:-3])
-                s.send(bytes('ok', encoding='utf-8'))
-            if word == "\n":
-                print('\n')
-                break
-s.close()
+def setup_connection(host, server_port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, server_port))
+    return s
+
+
+def chek_server_status():
+    data = s.recv(1024)
+    print (data.decode('utf-8'))
+    data = s.recv(1024)
+    print (data.decode('utf-8'))
+    s.send(bytes('Client connected', encoding='utf-8'))
+
+
+def get_data_from_server(s):
+    while True:
+        data = input("> ")
+        s.send(bytes(data, encoding='utf-8'))
+        if data == 'exit':
+            break
+        else:
+            while True:
+                word = s.recv(2048).decode('utf-8')
+                if word[-3:] == 'end':
+                    print(word[:-3])
+                    s.send(bytes('ok', encoding='utf-8'))
+                if word == "\n":
+                    print('\n')
+                    break
+
+if __name__ == '__main__':
+    host = input('Enter hostname:\n> ')
+    server_port =int(input('Enter server port:\n> '))
+    s = setup_connection(host, server_port)
+    chek_server_status()
+    get_data_from_server(s)
+    s.close()
--- server.py
+++ server.py
@@ -5,14 +5,16 @@ import sys
 from part_1 import get_data_from_file,  get_dict_trie_like, suggest_options
 
 
-if __name__ == "__main__":
-    path_to_file = input("Enter path to dictionary:\n> ")
-    server_port = int(input("Enter port:\n> "))
+def setup_server(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '127.50.50.1'
-    port = (host, server_port)
-    s.bind(port)
+    host = 'localhost'
+    server_adress = (host, port)
+    s.bind(server_adress)
     s.listen(1)
+    return s
+
+
+def preparing_server():
     conn, addr = s.accept()
     conn.send(bytes("Loading", encoding='utf-8'))
     prefixes_dict = suggest_options(get_dict_trie_like(get_data_from_file(
@@ -20,6 +22,10 @@ if __name__ == "__main__":
     conn.send(bytes("Server is ready", encoding='utf-8'))
     data = conn.recv(1024).decode('utf-8')
     print(data)
+    return prefixes_dict, conn
+
+
+def client_request_processing(conn):
     while True:
         data = conn.recv(1024).decode('utf-8')
         if data == 'exit':
@@ -29,4 +35,11 @@ if __name__ == "__main__":
                 conn.send(bytes(word_info[0] + 'end', encoding='utf-8'))
                 conn.recv(1024).decode('utf-8')
             conn.send(bytes('\n', encoding='utf-8'))
+
+
+if __name__ == "__main__":
+    path_to_file = input("Enter path to dictionary:\n> ")
+    s = setup_server(int(input("Enter port:\n> ")))
+    prefixes_dict, conn = preparing_server()
+    client_request_processing(conn)
     conn.close()
