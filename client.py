f030c8b39d80ceb20eb3325e6d77afa0ef0d8a21
Aleksei Shuliatev <ashuliatyev@gmail.com>
2017-09-16 23:40:44 +0300

    after pep8 checks

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
 import socket
-import sys
 
 
 # Function configures connection parametrs
 def setup_connection(host, server_port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# connection is TCP/IPv4
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # connection is TCP/IPv4
     s.connect((host, server_port))
     return s
 
@@ -12,10 +11,10 @@ def setup_connection(host, server_port):
 # Function checks server status
 def check_server_status():
     data = s.recv(1024)
-    print (data.decode('utf-8'))
+    print(data.decode('utf-8'))
     data = s.recv(1024)
-    print (data.decode('utf-8'))
-    s.send(bytes('Client connected', encoding='utf-8'))# sending to server, that client connected
+    print(data.decode('utf-8'))
+    s.send(bytes('Client connected', encoding='utf-8'))  # sending to server, that client connected
 
 
 # Function gets data from server and print it
@@ -23,22 +22,22 @@ def get_data_from_server(s):
     while True:
         data = input("> ")
         s.send(bytes(data, encoding='utf-8'))
-        if data == 'exit':# if client wants to disconnect, hi enter "exit"
+        if data == 'exit':  # if client wants to disconnect, hi enter "exit"
             break
         else:
             while True:
                 word = s.recv(2048).decode('utf-8')
                 if word[-3:] == 'end':
                     print(word[:-3])
-                    s.send(bytes('ok', encoding='utf-8'))# client sends message to serve, when gets all data
-                if word == "\n":# data flow ended when server send "\n" then the 
+                    s.send(bytes('ok', encoding='utf-8'))  # client sends message to serve, when gets all data
+                if word == "\n":  # data flow ended when server send "\n"
                     print('\n')
                     break
 
 if __name__ == '__main__':
     host = input('Enter hostname:\n> ')
-    server_port =int(input('Enter server port:\n> '))
+    server_port = int(input('Enter server port:\n> '))
     s = setup_connection(host, server_port)
-    chek_server_status()
+    check_server_status()
     get_data_from_server(s)
     s.close()
--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import socket
-import sys
 
 
 from part_1 import get_data_from_file,  get_dict_trie_like, suggest_options
 
+
 # Function configures the server
 def setup_server(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# connection is TCP/IPv4
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # connection is TCP/IPv4
     host = 'localhost'
     server_adress = (host, port)
     s.bind(server_adress)
@@ -16,12 +16,12 @@ def setup_server(port):
 
 # Function prepares server for client request processing
 def preparing_server(path_to_file):
-    conn, addr = s.accept()
-    conn.send(bytes("Loading", encoding='utf-8'))# sending server status to the client
+    conn = s.accept()[0]  # waiting for client connection
+    conn.send(bytes("Loading", encoding='utf-8'))  # sending server status to the client
     prefixes_dict = suggest_options(get_dict_trie_like(get_data_from_file(
-        path_to_file)), printing_option=False) # prepare dictionary
-    conn.send(bytes("Server is ready", encoding='utf-8'))# sending server status to the client
-    data = conn.recv(1024).decode('utf-8')# getting message from client, when he connected
+        path_to_file)), printing_option=False)  # prepare dictionary
+    conn.send(bytes("Server is ready", encoding='utf-8'))  # sending server status to the client
+    data = conn.recv(1024).decode('utf-8')  # getting message from client, when he connected
     print(data)
     return prefixes_dict, conn
 
@@ -30,13 +30,13 @@ def preparing_server(path_to_file):
 def client_request_processing(conn):
     while True:
         data = conn.recv(1024).decode('utf-8')
-        if data == 'exit':# if client sends "exit", server turn off
+        if data == 'exit':  # if client sends "exit", server turn off
             break
         else:
             for word_info in prefixes_dict.get(data):
-                conn.send(bytes(word_info[0] + 'end', encoding='utf-8'))# "end" says to client that all data was obtained
-                conn.recv(1024).decode('utf-8')# server gets answer from client, when client gets all data
-            conn.send(bytes('\n', encoding='utf-8'))# server sending "\n", when request processing is ending
+                conn.send(bytes(word_info[0] + 'end', encoding='utf-8'))  # "end" says to client that all data was obtained
+                conn.recv(1024).decode('utf-8')  # server gets answer from client, when client gets all data
+            conn.send(bytes('\n', encoding='utf-8'))  # server sending "\n", when request processing is ending
 
 
 if __name__ == "__main__":
