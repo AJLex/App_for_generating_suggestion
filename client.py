3e09d6f30215b857963a95e1b115a8e42fead3f8
Aleksei Shuliatev <ashuliatyev@gmail.com>
2017-09-16 23:30:00 +0300

    added comments

--- client.py
+++ client.py
@@ -2,33 +2,36 @@ import socket
 import sys
 
 
+# Function configures connection parametrs
 def setup_connection(host, server_port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# connection is TCP/IPv4
     s.connect((host, server_port))
     return s
 
 
-def chek_server_status():
+# Function checks server status
+def check_server_status():
     data = s.recv(1024)
     print (data.decode('utf-8'))
     data = s.recv(1024)
     print (data.decode('utf-8'))
-    s.send(bytes('Client connected', encoding='utf-8'))
+    s.send(bytes('Client connected', encoding='utf-8'))# sending to server, that client connected
 
 
+# Function gets data from server and print it
 def get_data_from_server(s):
     while True:
         data = input("> ")
         s.send(bytes(data, encoding='utf-8'))
-        if data == 'exit':
+        if data == 'exit':# if client wants to disconnect, hi enter "exit"
             break
         else:
             while True:
                 word = s.recv(2048).decode('utf-8')
                 if word[-3:] == 'end':
                     print(word[:-3])
-                    s.send(bytes('ok', encoding='utf-8'))
-                if word == "\n":
+                    s.send(bytes('ok', encoding='utf-8'))# client sends message to serve, when gets all data
+                if word == "\n":# data flow ended when server send "\n" then the 
                     print('\n')
                     break
 
--- server.py
+++ server.py
@@ -4,9 +4,9 @@ import sys
 
 from part_1 import get_data_from_file,  get_dict_trie_like, suggest_options
 
-
+# Function configures the server
 def setup_server(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# connection is TCP/IPv4
     host = 'localhost'
     server_adress = (host, port)
     s.bind(server_adress)
@@ -14,32 +14,34 @@ def setup_server(port):
     return s
 
 
-def preparing_server():
+# Function prepares server for client request processing
+def preparing_server(path_to_file):
     conn, addr = s.accept()
-    conn.send(bytes("Loading", encoding='utf-8'))
+    conn.send(bytes("Loading", encoding='utf-8'))# sending server status to the client
     prefixes_dict = suggest_options(get_dict_trie_like(get_data_from_file(
-        path_to_file)), printing_option=False)
-    conn.send(bytes("Server is ready", encoding='utf-8'))
-    data = conn.recv(1024).decode('utf-8')
+        path_to_file)), printing_option=False) # prepare dictionary
+    conn.send(bytes("Server is ready", encoding='utf-8'))# sending server status to the client
+    data = conn.recv(1024).decode('utf-8')# getting message from client, when he connected
     print(data)
     return prefixes_dict, conn
 
 
+# Function processing client request
 def client_request_processing(conn):
     while True:
         data = conn.recv(1024).decode('utf-8')
-        if data == 'exit':
+        if data == 'exit':# if client sends "exit", server turn off
             break
         else:
             for word_info in prefixes_dict.get(data):
-                conn.send(bytes(word_info[0] + 'end', encoding='utf-8'))
-                conn.recv(1024).decode('utf-8')
-            conn.send(bytes('\n', encoding='utf-8'))
+                conn.send(bytes(word_info[0] + 'end', encoding='utf-8'))# "end" says to client that all data was obtained
+                conn.recv(1024).decode('utf-8')# server gets answer from client, when client gets all data
+            conn.send(bytes('\n', encoding='utf-8'))# server sending "\n", when request processing is ending
 
 
 if __name__ == "__main__":
     path_to_file = input("Enter path to dictionary:\n> ")
     s = setup_server(int(input("Enter port:\n> ")))
-    prefixes_dict, conn = preparing_server()
+    prefixes_dict, conn = preparing_server(path_to_file)
     client_request_processing(conn)
     conn.close()
